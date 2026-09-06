981e36661980240d7101d1829c5d20c9c5db4880
Sidikh14 <sidikhsene43@gmail.com>
2026-09-06 21:04:18 +0000

    changement du dossier Sidikh Stock

--- plateforme-commercants/backend/src/server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const app = require('./app');
-
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-  console.log(`API commerçants démarrée sur le port ${PORT}`);
-});
